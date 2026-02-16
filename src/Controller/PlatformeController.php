8a885490209d0a3185d1e1da997e733df968c278
Olivier RATIEVILLE <olivier.ratieville@gmail.com>
2026-02-16 12:33:17 +0100

    Bug hunting

--- src/Controller/PlatformeController.php
+++ src/Controller/PlatformeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Platforme;
 use App\Repository\FilmRepository;
 use App\Repository\PlatformeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,7 +20,7 @@ final class PlatformeController extends AbstractController
     }
 
     #[Route('/platformes/{id}', name: 'app_platforme_show')]
-    public function show(FilmRepository $filmsRepository, PlatformeRepository $platforme): Response
+    public function show(Platforme $platforme): Response
     {
         return $this->render('platforme/show.html.twig', [
             'platforme' => $platforme,
